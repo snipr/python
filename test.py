24741991ad95fdae0bbc04e14b463886485abfe6
obaydallah <habuobaydallah@gmail.com>
2023-12-09 10:22:07 +0200

    test file with string methods

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # the first test programming by python
 
-myString= "******** i love python *******          "
+myString= "******** i love python lang *******"
 
 print(len(myString))
 print(myString.strip("* "))
@@ -9,3 +9,10 @@ myString2= "i-love-pYTHon-and-php-and-msql"
 print(myString2.rsplit("-",2))
 print(myString2.center(50,"*"))
 print(myString2.swapcase())
+print(myString.strip("*"))
+li=myString.strip("*").split()
+print(li[0].center(10,"@"))
+print(li)
+print(len(myString))
+my4="@".join(li)
+print(my4)
