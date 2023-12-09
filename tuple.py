7eef4c3fb3809d1b871147fb60c2bab8249eedc0
obaydallah <habuobaydallah@gmail.com>
2023-12-09 21:34:03 +0200

    tuple learn file

---
+++ tuple.py
@@ -0,0 +1,13 @@
+# Tuples methods
+a=(1,2,4,2,6)
+r=(5,3,2,3,6)
+b=(1,5,"a","t")
+print(type(a))
+print(type(b))
+c=a+b
+
+print(c)
+
+e,y,d,w,y=a
+print(d)
+print(f"the number of index:{b.index(5)}={b[1]}")
