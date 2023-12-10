7c65bbf43fdad9224f0101be11bf9c64719c60d4
obaydallah <habuobaydallah@gmail.com>
2023-12-10 12:07:27 +0200

    Dictianory file

---
+++ dictanary1.py
@@ -0,0 +1,16 @@
+lang={
+    "python":"best",
+    "html":"easy",
+    "php":"strong"
+}
+user={
+    "lang":lang,
+    "salary":6000,
+    "level":"advance"
+}
+user.update({"rank":5})
+
+print(user.items())
+#print(user)
+#print(user.keys())
+#print(user.values())
