724f6e1514ccc94ec427734222ac836c007fb4ae
obaydallah <habuobaydallah@gmail.com>
2023-12-11 11:11:01 +0200

    exsample file

---
+++ exsample.py
@@ -0,0 +1,6 @@
+firstName=input("Enter Your First Name: ").strip().capitalize()
+midName=input("Enter Your Mid Name: ").strip().capitalize()
+lastName=input("Enter Your Last Name: ").strip().capitalize()
+email=input("Enter Your Email: ").strip()
+print(f"Welcome {firstName} {midName:.1s} {lastName}")
+print(f"Your Domain is: {email[:email.index('@')]} and website is {email[email.index('@')+1:]}")
