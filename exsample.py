fe88f36a512a7333206c956f78739786b3312b82
obaydallah <habuobaydallah@gmail.com>
2023-12-11 11:34:31 +0200

    exsample file and if condition

--- exsample.py
+++ exsample.py
@@ -2,5 +2,12 @@ firstName=input("Enter Your First Name: ").strip().capitalize()
 midName=input("Enter Your Mid Name: ").strip().capitalize()
 lastName=input("Enter Your Last Name: ").strip().capitalize()
 email=input("Enter Your Email: ").strip()
-print(f"Welcome {firstName} {midName:.1s} {lastName}")
-print(f"Your Domain is: {email[:email.index('@')]} and website is {email[email.index('@')+1:]}")
+country=input("Enter Your Country: ").strip().capitalize()
+if country == "Palitine" :
+      print(f"Welcome {firstName} {midName:.1s} {lastName} palstine")
+      print(f"Your Domain is: {email[:email.index('@')]} and website is {email[email.index('@')+1:]}")
+elif country == "Jordan":
+      print(f"Welcome {firstName} {midName:.1s} {lastName} Joradan")
+      print(f"Your Domain is: {email[:email.index('@')]} and website is {email[email.index('@')+1:]}")
+else:
+      print("hiiiii")
